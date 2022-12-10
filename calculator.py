b9543291455a71fa8308ed9c78c9c6220127cb3a
I-Minevski <114011447+I-Minevski@users.noreply.github.com>
2022-12-10 16:38:01 +0200

    Change sign button added

--- calculator.py
+++ calculator.py
@@ -58,6 +58,15 @@ def divide():
     e.delete(0, END)
 
 
+def change_sign():
+    result = float(e.get())*-1
+    e.delete(0, END)
+    if result.is_integer():
+        e.insert(0, int(result))
+    else:
+        e.insert(0, result)
+
+
 def equals_to():
     if operation == 'add':
         result = first_number + float(e.get())
@@ -92,8 +101,6 @@ def equals_to():
             e.insert(0, result)
 
 
-
-
 button7 = Button(root, text='7', padx=40, pady=20, command=lambda: button_click(7)).grid(row=2, column=0)
 button8 = Button(root, text='8', padx=40, pady=20, command=lambda: button_click(8)).grid(row=2, column=1)
 button9 = Button(root, text='9', padx=40, pady=20, command=lambda: button_click(9)).grid(row=2, column=2)
@@ -110,10 +117,12 @@ button_AC = Button(root, text='AC', padx=35, pady=20, command=lambda: button_cle
 button0 = Button(root, text='0', padx=40, pady=20, command=lambda: button_click(0)).grid(row=5, column=1)
 button_point = Button(root, text='.', padx=41.5, pady=20, command=lambda: button_decimal()).grid(row=5, column=2)
 
-button_divide = Button(root, text='÷', padx=38, pady=20, command=lambda: divide()).grid(row=1, column=3)
-button_multiply = Button(root, text='×', padx=40, pady=20, command=lambda: multiply()).grid(row=2, column=3)
+button_divide = Button(root, text='÷', padx=39, pady=20, command=lambda: divide()).grid(row=1, column=3)
+button_multiply = Button(root, text='×', padx=38, pady=20, command=lambda: multiply()).grid(row=2, column=3)
 button_subtract = Button(root, text='-', padx=40, pady=20, command=lambda: subtract()).grid(row=3, column=3)
 button_add = Button(root, text='+', padx=38, pady=20, command=lambda: add()).grid(row=4, column=3)
 button_equals = Button(root, text='=', padx=38, pady=20, command=lambda: equals_to()).grid(row=5, column=3)
+
+button_change_sign = Button(root, text='+/-', padx=34, pady=20, command=lambda: change_sign()).grid(row=1, column=2)
 
 root.mainloop()
